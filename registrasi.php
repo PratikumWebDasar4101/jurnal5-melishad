7b7f938fdc1f3ce95280b82de903abd5dbbf99bb
melishad <42725372+melishad@users.noreply.github.com>
2018-10-01 10:33:03 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -4,7 +4,28 @@
 	<title>Form Registrasi</title>
 </head>
 <body>
-
+	<center>
+<h3>APLIKASI PENDAFTARAN MAHASISWA</h3>
+	<form action="prosesregis.php" method="post">		
+		<table>
+			<tr>
+				<td>Nim : </td>
+				<td><input type="text" name="nim"  pattern="[0-9]{10}" title="Nim harus angka dan 10 karakter"  required ></td>					
+			</tr>	
+			<tr>
+				<td>Nama : </td>
+				<td><input type="text" name="nama" pattern="[A-Za-z ]{1,25}" title=" Nama harus maks 25 karakter" required></td>					
+			</tr>	
+			<tr>
+				<td>Email : </td>
+				<td><input type="email" name="email" placeholder="email@gmail.com" required></td>					
+			</tr>	
+			<tr>
+				<td></td>
+				<td><br><input type="submit" name="submit" value="Simpan"></td>					
+			</tr>				
+		</table>
+	</form>
 
 
 </body>
