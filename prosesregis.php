f95b362dbdb8581ccc6622916e8343f1d564c6fb
melishad <42725372+melishad@users.noreply.github.com>
2018-10-01 10:33:37 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,75 @@
+<?php 
+session_start();
+if (isset($_POST['submit'])) {
+	if (!isset($_SESSION['user'])) {
+		$user = array();
+	}else{
+		$user = $_SESSION['user'];
+	}
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];
+	$email = $_POST['email'];
+	$userbaru = array (
+					'nim' => $nim,
+					'nama' => $nama,
+					'email' => $email
+					);
+	array_push($user, $userbaru);
+	$_SESSION['user'] = $user;
+	
+}
+
+ ?>
+<table>
+
+ <tr>
+ 	<th>NIM</th>
+ </tr>
+ <?php  
+$user = $_SESSION['user'];
+$num = 0;
+	while ($num < count ($user)) {
+ ?>
+ <tr>
+ 	<td> <?php  echo $user[$num] ['nim']; ?></td>
+ </tr>
 <?php
-
-
-
-
-
+$num++;
+} 
 ?>
+	<tr>
+		<th>NAMA</th>
+	</tr>
+
+<?php 
+$user = $_SESSION['user'];
+$num = 0;
+	while ($num < count ($user)) {
+ ?>
+ <tr>
+ 	<td><?php echo $user[$num]['nama']; ?></td>
+ </tr>
+ <?php
+ $num++;
+}
+ ?>
+<tr>
+	<th>Email</th>
+</tr>
+<?php 
+$user = $_SESSION['user'];
+$num = 0;
+	while ($num < count ($user)) {
+ ?>
+
+ <tr>
+ 	<td><?php echo $user[$num] ['email']; ?></td>
+ </tr>
+
+<?php 
+$num++;
+}
+ ?>
+</table>
+<br>
+<a href="registrasi.php">TAMBAH DATA BARU</a>
